a740fda83a8c9b09156fb2fc639354239c4dec34
AmeniMaamouri <amanimaamouri@gmail.com>
2023-12-23 12:27:51 +0100

    refactoring code

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('First app nodejs \n');
+   response.end('Created By Ameni Maamouri , Azure App \n');
 }).listen(8080);
 
 // Console will print the message
